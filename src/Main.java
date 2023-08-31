a85e9ad6cffdb2645f30b26e35e36686fa4c92d9
progTD <corowainy@yandex.ru>
2023-08-31 21:18:57 +0300

    commit for PR

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,8 @@ public class Main {
         Truck truck = new Truck("truck1", 6);
         Truck truck2 = new Truck("truck2", 8);
 
-
         Bicycle bicycle = new Bicycle("bicycle1", 2);
         Bicycle bicycle2 = new Bicycle("bicycle2", 2);
-
 
         ServiceStation station = new ServiceStation();
         station.check(car);
