75da891fa18b1ea9f05988e0340a4b4e5bc4775c
progTD <corowainy@yandex.ru>
2023-09-01 16:41:11 +0300

    fixed bugs

--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,4 @@
 public class Car extends Vehicle implements ServiceTyre, ServiceEngine{
-
-
-    @Override
-    public void checkEngine() {
-        ServiceEngine.super.checkEngine();
-    }
-
-    @Override
-    public void updateTyre() {
-        ServiceTyre.super.updateTyre();
-    }
-
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
--- src/Truck.java
+++ src/Truck.java
@@ -3,19 +3,4 @@ public class Truck extends Vehicle implements ServiceEngine, ServiceTyre, Servic
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
-
-    @Override
-    public void checkEngine() {
-        ServiceEngine.super.checkEngine();
-    }
-
-    @Override
-    public void checkTrailer() {
-        ServiceTrailer.super.checkTrailer();
-    }
-
-    @Override
-    public void updateTyre() {
-        ServiceTyre.super.updateTyre();
-    }
 }
